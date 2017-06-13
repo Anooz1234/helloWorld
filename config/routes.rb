deb28fbb6d2e5d2fe44642220d503fd027f62c71
Anooz1234 <anooz.chaos@gmail.com>
2017-06-13 22:05:28 +0000

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #this is called using the application controller hello
+  root 'users#index'
+end
